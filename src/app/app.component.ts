35beb9b9d27072fd4e060f9a8a405a247687aee0
Fabricio Varisco <fabricio.varisco@outlook.com>
2024-03-29 18:59:54 -0300

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,56 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PhaserGame } from '../game/phaser-game.component';
 import { MainMenu } from '../game/scenes/MainMenu';
 import { CommonModule } from '@angular/common';
 import { EventBus } from '../game/EventBus';
 import Player from '../game/Player';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, PhaserGame],
-    templateUrl: './app.component.html'
+    imports: [CommonModule, RouterOutlet, PhaserGame, ReactiveFormsModule],
+    templateUrl: './app.component.html',
 })
 export class AppComponent implements AfterViewInit
 {
 
-    public PlayerData: Player = new Player("Name", 0, [], []);
+    public PlayerData = signal<Player>(new Player("Name", 0, [], []));
+    
     private _openItemsPanel: boolean = false;
     private _openMenuPanel: boolean = false;
 
 
-    @ViewChild(PhaserGame) phaserRef!: PhaserGame;
+@ViewChild(PhaserGame) phaserRef!: PhaserGame;
 
-    ngAfterViewInit()
+ngAfterViewInit()
+{
+    EventBus.on('current-scene-ready', (scene: Phaser.Scene) =>
     {
-        EventBus.on('current-scene-ready', (scene: Phaser.Scene) => {
-            console.log(this.phaserRef.scene)
+        console.log(this.phaserRef.scene)
 
-        });
-        
-    }
+    });
+
+}
+
+ngOnInit(){
+    // fetch('/player').then(res => res.json())
+    // .then(data =>data)
+}
 
     public changeScene()
+{
+
+    if (this.phaserRef.scene)
     {
 
-        if (this.phaserRef.scene)
-        {
-
-            const scene = this.phaserRef.scene as MainMenu;
-            scene.changeScene();
-            // if this.phaserRef.scene.scene.key
-            console.log(this.phaserRef.scene.scene.key)
-        }
-
+        const scene = this.phaserRef.scene as MainMenu;
+        scene.changeScene();
+        // if this.phaserRef.scene.scene.key
+        console.log(this.phaserRef.scene.scene.key)
     }
+
+}
 
 }
